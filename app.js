878d97f31d69fde4ec12c81a02f37f8b428ecdad
Thanh Ta <mth.1012.ta@gmail.com>
2022-09-14 21:46:56 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const blogRoutes = require('./route/blog.js');
+const dashboardRoutes = require('./route/dashboard.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/', blogRoutes);
+app.use('/dashboard', dashboardRoutes);
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3001...')
+})
