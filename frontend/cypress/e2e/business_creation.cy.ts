a784e8cb32194856ab51687355156712f02a0f69
p-carrier <p-carrier@users.noreply.github.com>
2022-09-28 18:32:00 -0400

    PAPILIO-43 create integration test for feature

--- frontend/cypress/e2e/business_creation.cy.ts
+++ frontend/cypress/e2e/business_creation.cy.ts
@@ -1,7 +1,33 @@
 /// <reference types="cypress" />
 
-describe("empty spec", () => {
-  it("passes", () => {
-    cy.visit("https://example.cypress.io");
+describe("visit home page", () => {
+  const companyName = "business123";
+
+  afterEach(() => {
+    // Delete business from database
+    cy.request("DELETE", `business/${companyName}`);
+  });
+
+  it("when company name doesn't exist then company should be created", () => {
+    cy.visit("/get-started");
+
+    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
+
+    cy.url().should("include", "/admin");
+    cy.getCookie("session").should("exist");
+
+    cy.get("h1").should("contain", "Admin");
+  });
+
+  it("when company name already exist then an error message should be display", () => {
+    cy.visit("/get-started");
+    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
+    cy.url().should("include", "/admin");
+
+    cy.visit("/get-started");
+    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
+
+    cy.url().should("include", "/get-started");
+    cy.get("h1").should("contain", "Create");
   });
 });
