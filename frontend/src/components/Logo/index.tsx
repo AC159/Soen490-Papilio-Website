377c062b289cb48b856cee075ed74ac50e6df998
p-carrier <p-carrier@users.noreply.github.com>
2022-10-02 19:26:12 -0400

    create logo component

---
+++ frontend/src/components/Logo/index.tsx
@@ -0,0 +1,20 @@
+import classNames from "classnames";
+
+export declare type LogoInterface = {
+  hasText: boolean
+  size?: 'sm'|'md'|'lg'
+}
+
+const Logo = ({hasText=false, size='md'}: LogoInterface) => {
+  const className = {
+    'sm': 'h-6',
+    'md': 'h-10',
+    'lg': 'h-14'
+  };
+
+  const imgUrl = hasText ? `${process.env.PUBLIC_URL}/logoText.png` : `${process.env.PUBLIC_URL}/logo.png`;
+
+  return <img src={imgUrl} alt="papilio logo" className={className[size]}/>;
+};
+
+export default Logo;
