957e52806f8f6c50d56a851035e0eda36dad318e
p-carrier <p-carrier@users.noreply.github.com>
2022-10-02 19:26:35 -0400

    create button component

---
+++ frontend/src/components/Button/index.tsx
@@ -0,0 +1,22 @@
+import classNames from 'classnames';
+
+export declare type ButtonInterface = {
+  text: string;
+  primary?: boolean;
+  onClick: () => void;
+}
+
+const Button = ({text, onClick, primary=true}: ButtonInterface): JSX.Element => {
+  const className = classNames('rounded-md', 'flex', 'px-2', 'py-1', 'border-2', 'border-green-200', 'cursor-pointer', {
+    'bg-green-200': primary,
+    'bg-none': !primary,
+    'text-green-200': !primary,
+    'text-white': primary
+  });
+
+  return (
+    <div onClick={onClick} className={className}>{text}</div>
+  );
+};
+
+export default Button;
