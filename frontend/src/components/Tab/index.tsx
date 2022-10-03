b55555c267800b9240d0e7d57f7c1f3a44421fdf
p-carrier <p-carrier@users.noreply.github.com>
2022-10-03 00:09:38 -0400

    create Tab component

---
+++ frontend/src/components/Tab/index.tsx
@@ -0,0 +1,33 @@
+import classNames from "classnames";
+
+export declare type TabInterface = {
+  label: string;
+  onClick: () => void;
+  horizontal?: boolean;
+  isSelected: boolean;
+  icon?: 'ad'|'home'|'event'|'employee'
+};
+
+const Tab = ({label, isSelected, icon, horizontal=false, onClick}: TabInterface): JSX.Element => {
+  const className = classNames('flex py-2 px-3 items-center cursor-pointer', {
+    'border-l-4 flex-1': !horizontal,
+    'border-b-4 justify-center w-max': horizontal,
+    'border-transparent': !isSelected
+  });
+
+  const icons = {
+    'ad': 'confirmation_number',
+    'home': 'home',
+    'event': 'event_note',
+    'employee': 'Badge',
+  };
+  
+  return (
+    <div className={className} onClick={onClick}>
+      {icon && <span className="material-symbols-outlined text-base mr-3">{icons[icon]}</span>}
+      <span>{label}</span>
+    </div>
+  );
+};
+
+export default Tab;
