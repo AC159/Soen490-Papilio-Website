19d81b712c6e2e583270127520b65bc76582ebd6
glenpham <glen_pham@hotmail.com>
2022-11-06 17:15:59 -0500

    PAPILIO-32-and-37 Fixes
    Fixes made based on comments from @p-carrier

--- src/pages/Dashboard/Activities/ActivityTable/index.tsx
+++ src/pages/Dashboard/Activities/ActivityTable/index.tsx
@@ -1,6 +1,3 @@
-// import Button from '../../components/Button';
-// import { IconNames } from '../../components/Icon';
-// import SearchBar from '../SearchBar';
 import Row from '../../../../features/Table/Row';
 
 const Table = (): JSX.Element => {
@@ -11,6 +8,7 @@ const Table = (): JSX.Element => {
           <Row data={['Activity Title', 'Start Date', 'End Date', 'Location', 'Status']} head/>
         </thead>
         <tbody>
+          {/* TODO: Change from hardcoded information to fetching information from backend */}
           <Row data={['Laurentian Lanes Bowling', '01-10-2022', '01-11-2022', '222 Mnt de Liesse, Saint-Laurent, QC H4T 1N8', 'Expired']}/>
           <Row data={['Activity #2', '01-11-2022', '01-12-2022', '123 Guy Street, Montreal, QC', 'Active']}/>
           <Row data={['Activity #3', '01-01-2023', '01-02-2023', '123 Guy Street, Montreal, QC', 'Inactive']}/>
--- src/pages/Dashboard/Activities/AddForm/constant.ts
+++ src/pages/Dashboard/Activities/AddForm/constant.ts
@@ -20,15 +20,15 @@ export const INPUT_ACTIVITY_DESCRIPTION = 'activityDescription';
 export const INPUT_ACTIVITY_DESCRIPTION_LABEL = 'Description';
 export const INPUT_ACTIVITY_DESCRIPTION_PLACEHOLDER = 'Enter a description of your activity';
 
-export const INPUT_ACTIVITY_COST_INDV = 'activityCost';
+export const INPUT_ACTIVITY_COST_INDV = 'activityCostIndv';
 export const INPUT_ACTIVITY_COST_INDV_LABEL = 'Cost per Person';
 export const INPUT_ACTIVITY_COST_INDV_PLACEHOLDER = 'e.g. 00.00';
 
-export const INPUT_ACTIVITY_GROUP = 'activityCost';
+export const INPUT_ACTIVITY_GROUP = 'activityGroupSize';
 export const INPUT_ACTIVITY_GROUP_LABEL = 'Group Size';
 export const INPUT_ACTIVITY_GROUP_PLACEHOLDER = 'Enter group size for cost';
 
-export const INPUT_ACTIVITY_COST_GROUP = 'activityCost';
+export const INPUT_ACTIVITY_COST_GROUP = 'activityCostGroup';
 export const INPUT_ACTIVITY_COST_GROUP_LABEL = 'Cost per Group';
 export const INPUT_ACTIVITY_COST_GROUP_PLACEHOLDER = 'e.g. 00.00';
 
