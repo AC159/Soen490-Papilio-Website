7fbaf3acd69a680463a796ffb216b33c53a0f59c
p-carrier <p-carrier@users.noreply.github.com>
2023-01-13 15:47:49 -0500

    modify form header title text for better understanding

--- src/pages/Login/BusinessForm/constant.ts
+++ src/pages/Login/BusinessForm/constant.ts
@@ -1,4 +1,4 @@
-export const FORM_HEADING = 'How would you like to name your business?';
+export const FORM_HEADING = 'What will be your business alias?';
 export const SUBMIT_BUTTON_TEXT = 'Create business';
 export const INPUT_BUSINESS_ID = 'businessId';
 export const INPUT_BUSINESS_PLACEHOLDER = 'Insert your business ID';
