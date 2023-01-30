b2f557dd0d9d1eeb57012a36c0fa28127a5a5ebc
p-carrier <p-carrier@users.noreply.github.com>
2023-01-29 19:02:49 -0500

    PAPILIO-88 insert tests to DeleteForm

---
+++ src/pages/Dashboard/Employees/DeleteForm/index.spec.tsx
@@ -0,0 +1,79 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import DeleteForm from '.';
+import * as constant from './constant';
+import * as Table from '../../../../features/Table';
+
+// jest.mock('../../../../features/Table', () => ({
+//   __esModule: true,
+//   default: jest.fn(),
+// }));
+
+const defaultProps = {
+  employees: [],
+  onSubmit: () => {},
+};
+
+const oneEmployee = [
+  {
+    id: 'a',
+    name: 'A name',
+    email: 'fake@email.com',
+    role: 'Admin',
+  },
+];
+
+describe('Delete Form', () => {
+  it('displays the form headline', () => {
+    render(<DeleteForm {...defaultProps} />);
+    expect(screen.getByText(constant.FORM_HEADLINE)).toBeInTheDocument();
+  });
+
+  it('displays a button to delete the employees', () => {
+    render(<DeleteForm {...defaultProps} />);
+    expect(screen.getByText(constant.BUTTON_TEXT)).toBeInTheDocument();
+  });
+
+  it('calls onSubmit when the user clicks the delete button', async () => {
+    const mockOnSubmit = jest.fn();
+    render(<DeleteForm {...defaultProps} onSubmit={mockOnSubmit} />);
+
+    userEvent.click(screen.getByText(constant.BUTTON_TEXT));
+
+    expect(mockOnSubmit).toHaveBeenCalled();
+  });
+
+  it('passes the employees to the table components', () => {
+    jest.spyOn(Table, 'default');
+
+    render(<DeleteForm {...defaultProps} employees={oneEmployee} />);
+
+    expect(Table.default).toHaveBeenCalledWith(
+      expect.objectContaining({
+        employees: oneEmployee,
+      }),
+      {},
+    );
+  });
+
+  it('insert the ids of deleted employees when click on user row', () => {
+    const mockOnSubmit = jest.fn();
+    render(<DeleteForm employees={oneEmployee} onSubmit={mockOnSubmit} />);
+
+    userEvent.click(screen.getByRole('checkbox'));
+    userEvent.click(screen.getByText(constant.BUTTON_TEXT));
+
+    expect(mockOnSubmit).toHaveBeenCalledWith(['a']);
+  });
+
+  it('removes the id of the deleted employee when double click on user row', () => {
+    const mockOnSubmit = jest.fn();
+    render(<DeleteForm employees={oneEmployee} onSubmit={mockOnSubmit} />);
+
+    userEvent.dblClick(screen.getByRole('checkbox'));
+    userEvent.click(screen.getByText(constant.BUTTON_TEXT));
+
+    expect(mockOnSubmit).toHaveBeenCalledWith([]);
+  });
+});
