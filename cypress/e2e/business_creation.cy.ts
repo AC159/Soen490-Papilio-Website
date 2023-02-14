dbc3e693c4945b6c4961064051381b56e5382c23
p-carrier <p-carrier@users.noreply.github.com>
2023-02-14 16:37:36 -0500

    create automated acceptance test for the website

--- cypress/e2e/business_creation.cy.ts
+++ cypress/e2e/business_creation.cy.ts
@@ -1,33 +1,79 @@
 /// <reference types="cypress" />
 
-describe("visit home page", () => {
-  const companyName = "business123";
+describe('Papilio Website', () => {
+  const companyName = 'business123';
+  const fakeEmail = 'fake@email.com';
+  const fakePassword = 'password';
+  const fakeCompany = 'ABC';
 
   afterEach(() => {
     // Delete business from database
-    cy.request("DELETE", `business/${companyName}`);
+    // cy.request("DELETE", `business/${companyName}`);
   });
 
-  it("when company name doesn't exist then company should be created", () => {
-    cy.visit("/get-started");
+  const loggedInAdminEmployee = () => {
+    cy.visit('/login');
+    cy.get('#email').type(fakeEmail);
+    cy.get('#password').type(fakePassword);
+    cy.get('#businessId').type(fakeCompany);
+    cy.get('button').click();
+  };
 
-    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
-
-    cy.url().should("include", "/admin");
-    cy.getCookie("session").should("exist");
-
-    cy.get("h1").should("contain", "Admin");
+  it('display home page with the company logo on start up', () => {
+    cy.visit('/');
+    cy.contains('Welcome to Papilio');
   });
 
-  it("when company name already exist then an error message should be display", () => {
-    cy.visit("/get-started");
-    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
-    cy.url().should("include", "/admin");
+  it('visit login from home page', () => {
+    cy.visit('/');
+    cy.contains('Login').click();
+    cy.url().should('include', '/login');
+  });
 
-    cy.visit("/get-started");
-    cy.get("input[name=businessId]").type(`${companyName}{enter}`);
+  it('login by provinding the correct credentials', () => {
+    cy.visit('/login');
+    cy.get('#email').type(fakeEmail);
+    cy.get('#password').type(fakePassword);
+    cy.get('#businessId').type(fakeCompany);
+    cy.get('button').click();
+    cy.url().should('include', `/${fakeCompany}/dashboard`);
+  });
 
-    cy.url().should("include", "/get-started");
-    cy.get("h1").should("contain", "Create");
+  it('logs out an employee', () => {
+    loggedInAdminEmployee();
+    cy.contains('Logout').click();
+    cy.url().should('equal', `${Cypress.config().baseUrl}/`);
+  });
+
+  it('displays the business activities to a logged in employee', () => {
+    loggedInAdminEmployee();
+    cy.contains('Activity Manager').click();
+
+    cy.url().should('include', `${fakeCompany}/dashboard/activities`);
+    cy.contains('Just for fun');
+  });
+
+  it('displays the business employee to a logged in employee', () => {
+    loggedInAdminEmployee();
+    cy.contains('Employees').click();
+    cy.url().should('include', `${fakeCompany}/dashboard/employees`);
+    cy.contains('Sam');
+  });
+
+  it("can't delete itself when logged in as an admin", () => {
+    loggedInAdminEmployee();
+    cy.contains('Employees').click();
+    cy.contains('Delete').click();
+
+    cy.contains(fakeEmail)
+      .parent()
+      .get('input[type=checkbox]')
+      .should('be.disabled');
+  });
+
+  it.skip('deletes an employee when user is an admin', () => {
+    loggedInAdminEmployee();
+    cy.contains('Employees').click();
+    cy.contains('Delete').click();
   });
 });
